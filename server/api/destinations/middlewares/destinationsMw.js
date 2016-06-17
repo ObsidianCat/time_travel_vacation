76dc2be8b2fddf39b6995daa81757b9f2222d332
Lula Leus <lula.leus@gmail.com>
2016-06-17 17:03:45 +0300

    Refactor: Callback find by id changed to promise

--- server/api/destinations/middlewares/destinationsMw.js
+++ server/api/destinations/middlewares/destinationsMw.js
@@ -1,23 +1,28 @@
 /**
  * Created by Lula on 6/10/2016.
  */
-// var Destination = require("../models/Destination");
+//
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+
 var destinationMiddlewares = function(Destination){
   var findDestination = function(req, res, next){
-    Destination.findById(req.params.destinationId, function(err, destination){
-      if(err){
+    Destination.findById(req.params.destinationId).exec()
+      .then(function(destination) {
+        if(destination){
+          req.destination = destination;
+          next();
+        }
+        else{
+          res.status(404).send("No destination found");
+        }
+      })
+      .catch(function(err){
         res.status(500).send(err);
-      }
-      else if(destination){
-        req.destination = destination;
-        next();
-      }
-      else{
-        res.status(404).send("No destination found");
-      }
-    })
+      });
   };
-  
+
+
   return {
     findDestination:findDestination
   }
