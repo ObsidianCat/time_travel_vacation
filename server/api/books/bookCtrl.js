4177af8e8b5cc33e5e6fd2cbf3a6d6ff9dd61f00
Lula Leus <lula.leus@gmail.com>
2016-06-28 11:21:03 +0300

    Feat: Book routes
    writing of logic for book resource is finished.

--- server/api/books/bookCtrl.js
+++ server/api/books/bookCtrl.js
@@ -5,22 +5,19 @@
 
 var bookCtrl = function(Book, Destination){
 
-  var post = function createDestination (req,res) {
+  var post = function createDestination (req,res, next) {
     var book = new Book(req.body);
     book.destination = req.destination;
     req.destination.books.push(book);
 
-    Promise.all([req.destination.save(), book.save()]).then(values => {
-      console.log(values[0], values[1]);
-      res.status(201);
-      res.send({"values": values});
+    Promise.all([req.destination.save(), book.save()]).then(data => {
+      console.log(data);
+      res.status(200).send({data:data[1]});
+    },
+     function(err) {
+       console.error("error occurred during book saving");
+       next(err);
     })
-      .catch(
-        function (err) {
-          console.error("error occurred during book saving");
-          res.send(err);
-        }
-      );
   };
 
   var getAll = function(req, res){
@@ -38,28 +35,18 @@ var bookCtrl = function(Book, Destination){
 
   var deleteOne = function(req, res, next){
 
-    Destination.update({_id:req.destination._id}, {$pull: {books: req.book._id}}, function(err, data){
-      if(err) {
-        return res.status(500).json({'error' : 'error in deleting address'});
+    req.destination.books.pull({ _id: req.book._id }); // removed
+    Promise.all([req.destination.save(), req.book.remove()]).then(values => {
+      console.log(values);
+      res.status(204);
+      res.send({"message": "book removed"});
+    })
+    .catch(
+      function (err) {
+        console.error("error occurred during book deleting");
+        next(err);
       }
-
-      req.book.remove();
-      res.json(data);
-
-    });
-
-    // req.destination.books.pull({ _id: req.book._id }); // removed
-    // Promise.all([req.destination.save(), req.book.remove()]).then(values => {
-    //   console.log(values);
-    //   res.status(201);
-    //   res.send({"message": "book removed"});
-    // })
-    // .catch(
-    //   function (err) {
-    //     console.error("error occurred during book deleting");
-    //     next(err);
-    //   }
-    // );
+    );
   };
 
   return{
