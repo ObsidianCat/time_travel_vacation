d4d3b25d4c14a172a47bcbe01b9dd68418ea4b6b
Lula Leus <lula.leus@gmail.com>
2016-07-03 13:07:41 +0300

    Feat: App angular component created

--- client/dev/index.ts
+++ client/dev/index.ts
@@ -1,6 +1,7 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';
 
-import {TodoCmp} from './todo/components/todo-cmp';
-
-bootstrap(TodoCmp, [HTTP_PROVIDERS]);
+// import {TodoCmp} from './todo/components/todo-cmp';
+// bootstrap(TodoCmp, [HTTP_PROVIDERS]);
+import { AppComponent } from './vacation-chooser/components/app.component';
+bootstrap(AppComponent, [HTTP_PROVIDERS]);
---
+++ client/dev/vacation-chooser/components/app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Lula on 7/3/2016.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'vacations-app',
+  templateUrl:'vacation-chooser/templates/app.component.html'
+
+})
+export class AppComponent {
+  name = "Time traveler";
+}
