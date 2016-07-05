72e1a1b3e78c939aced410bebb7ec608aa5dbfa2
Lula Leus <lula.leus@gmail.com>
2016-07-05 13:26:07 +0300

    Some generated files

--- client/dev/index.js
+++ client/dev/index.js
@@ -1,5 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var http_1 = require('@angular/http');
-var todo_cmp_1 = require('./todo/components/todo-cmp');
-platform_browser_dynamic_1.bootstrap(todo_cmp_1.TodoCmp, [http_1.HTTP_PROVIDERS]);
+// import {TodoCmp} from './todo/components/todo-cmp';
+// bootstrap(TodoCmp, [HTTP_PROVIDERS]);
+var app_component_1 = require('./vacation-chooser/components/app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
