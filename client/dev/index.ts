923360d617a52dab2171a740b6f720d670d880ee
Lula Leus <lula.leus@gmail.com>
2016-07-05 13:49:36 +0300

    Chore: Fixes after generator update

--- client/dev/index.ts
+++ client/dev/index.ts
@@ -1,7 +1,13 @@
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';
-
-// import {TodoCmp} from './todo/components/todo-cmp';
-// bootstrap(TodoCmp, [HTTP_PROVIDERS]);
 import { AppComponent } from './vacation-chooser/components/app.component';
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+
+import {TodoCmp} from './todo/components/todo-cmp';
+// bootstrap(AppComponent, [HTTP_PROVIDERS]);
+
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  disableDeprecatedForms(),
+  provideForms()
+]);
