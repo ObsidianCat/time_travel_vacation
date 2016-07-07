af5ee8d62ebffd3bc7f3d6af346e3d0b7d3c12a9
Lula Leus <lula.leus@gmail.com>
2016-07-07 10:44:03 +0300

    Chore: files ignoring

--- client/dev/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var http_1 = require('@angular/http');
-// import {TodoCmp} from './todo/components/todo-cmp';
-// bootstrap(TodoCmp, [HTTP_PROVIDERS]);
-var app_component_1 = require('./vacation-chooser/components/app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
