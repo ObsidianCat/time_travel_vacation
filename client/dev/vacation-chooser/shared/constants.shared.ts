dfeae140b8db5147b8888f4eb0f3ebb47e9fe80c
Lula Leus <lula.leus@gmail.com>
2016-07-10 12:43:40 +0300

    Feat: search by time period

--- client/dev/vacation-chooser/shared/constants.shared.ts
+++ client/dev/vacation-chooser/shared/constants.shared.ts
@@ -9,9 +9,14 @@ const ART_TAGS = [
 const HISTORY_TAGS = [
   "Early Middle Ages","The Renaissance", "Ancient Rome", "Ancient Greece"
 ];
+
+const TIME_RANGE = {
+  START:-3000,
+  END:2000
+};
 const DESTINATION_URLS = {
   ALL_DESTINATIONS: "api/destinations",
   RANDOM_DESTINATION: "/api/destinations?random=true",
 };
 
-export {ART_TAGS, HISTORY_TAGS, DESTINATION_URLS};
+export {ART_TAGS, HISTORY_TAGS, TIME_RANGE, DESTINATION_URLS};
