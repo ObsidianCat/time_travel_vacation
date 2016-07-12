92018db8b1901fb196367b1e4aa11c948b3be569
Lula Leus <lula.leus@gmail.com>
2016-07-12 14:30:10 +0300

    Feat: book data service added to providers list

--- client/dev/vacation-chooser/components/app.component.ts
+++ client/dev/vacation-chooser/components/app.component.ts
@@ -4,13 +4,14 @@
 import {Component} from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { DestinationDataHandlerService } from "../services/destination-data-handler.service";
-import { AdviceDataHandlerService } from "../services/Advice-data-handler.service";
+import { AdviceDataHandlerService } from "../services/advice-data-handler.service";
+import { BookDataHandlerService } from "../services/book-data-handler.service";
 
 @Component({
   selector: 'vacations-app',
   templateUrl:'vacation-chooser/templates/app.component.html',
   directives: [ROUTER_DIRECTIVES],
-  providers:[DestinationDataHandlerService, AdviceDataHandlerService]
+  providers:[DestinationDataHandlerService, AdviceDataHandlerService, BookDataHandlerService]
 
 })
 export class AppComponent {
