78758e42c5eedca35d743de75dca1eec8c9ae10d
Lula Leus <lula.leus@gmail.com>
2016-07-16 13:59:31 +0300

    First deploy

--- server/api/middlewares/commmonMw.js
+++ server/api/middlewares/commmonMw.js
@@ -5,7 +5,7 @@
 
 var Destination = require("../destinations/models/Destination");
 var Book = require("../books/models/Book");
-var Advice = require("../advices/models/advice");
+var Advice = require("../advices/Models/advice");
 var User = require("../users/models/User");
 
 function findItem(req, res, next, id, name){
