4300d7921baa4fdf1993c501e29084e10898abf4
Lula Leus <lula.leus@gmail.com>
2016-07-16 15:18:20 +0300

    Fix component names

--- client/dev/vacation-chooser/components/finder.component.ts
+++ client/dev/vacation-chooser/components/finder.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 import {ArtTagsFinderComponent } from './art-tags-finder.component';
 import { HistoryTagsFinderComponent } from './history-tags-finder.component';
 import { PeriodFinderComponent } from './period-finder.component';
-import {DestPreviewComponent } from './dest-preview.Component';
+import {DestPreviewComponent } from './dest-preview.component';
 @Component({
   templateUrl:'vacation-chooser/templates/finder.component.html',
   selector: 'finder',
