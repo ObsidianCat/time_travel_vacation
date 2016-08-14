72499e55da9ecf6611e95d8dd444d6964e58bbcb
Lula Leus <lula.leus@gmail.com>
2016-08-14 19:47:26 +0300

    Feat: adding Sass to project

--- tasks/index.js
+++ tasks/index.js
@@ -3,7 +3,7 @@ import gulp from 'gulp';
 import {tasks} from './client/const';
 
 gulp.task('default', [tasks.CLIENT_WATCH]);
-
+gulp.task('create_css',[tasks.CLIENT_SASS_TO_CSS]);
 require('require-dir')('client');
 
 
