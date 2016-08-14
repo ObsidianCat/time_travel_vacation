f94b2484cee6ecfbc7015f3625f7d5d729e3a3c0
Lula Leus <lula.leus@gmail.com>
2016-08-14 19:55:37 +0300

    Feat: Adding Sass to project

--- tasks/client/sass_to_css.js
+++ tasks/client/sass_to_css.js
@@ -5,10 +5,11 @@ import gulp from 'gulp';
 import {path, tasks} from './const';
 import sass from 'gulp-sass';
 
-const STYLES_SOURCE = path.DEV + path.APP_NAME+'styles/scss/test.scss';
-const STYLES_DESTINATION = path.DEV + path.APP_NAME+'styles/css/';
+const STYLES_SOURCE = path.DEV + path.APP_NAME+'/styles/scss/*.scss';
+const STYLES_DESTINATION = path.DEV + path.APP_NAME+'/styles/css/';
 
 gulp.task(tasks.CLIENT_SASS_TO_CSS, function() {
+  console.log(STYLES_SOURCE, STYLES_DESTINATION);
   return gulp.src(STYLES_SOURCE)
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest(STYLES_DESTINATION));
