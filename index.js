e8a03b2a3f0314f918af2c7928f5acca239af38d
Lula Leus <lula.leus@gmail.com>
2016-09-10 14:37:12 +0300

    Feature: update angular 2 from rc4 to rc6

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-// import { BrowserModule  } from '@angular/platform-browser';
-// import { FormsModule }   from '@angular/forms';
-//
-//
-// import {disableDeprecatedForms, provideForms} from '@angular/forms';
-// import {bootstrap} from '@angular/platform-browser-dynamic';
-// import {HTTP_PROVIDERS} from '@angular/http';
-// import { APP_ROUTER_PROVIDERS } from './vacation-chooser/app.routes';
-//
-//
-// bootstrap(AppComponent, [
 "use strict";
 const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+const app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
