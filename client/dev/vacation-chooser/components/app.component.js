ad4a1a82eacdbd846f9198165f01e07c6e2de0bc
Lula Leus <lula.leus@gmail.com>
2016-10-15 11:15:56 +0300

    Feat: wiring up Auth0

--- client/dev/vacation-chooser/components/app.component.js
+++ client/dev/vacation-chooser/components/app.component.js
@@ -12,16 +12,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  * Created by Lula on 7/3/2016.
  */
 var core_1 = require('@angular/core');
+var auth_service_1 = require('../services/auth.service');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(auth) {
+        this.auth = auth;
         this.name = "Time traveler";
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'vacations-app',
             templateUrl: 'vacation-chooser/templates/app.component.html',
+            providers: [auth_service_1.Auth],
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [auth_service_1.Auth])
     ], AppComponent);
     return AppComponent;
 }());
--- client/dev/vacation-chooser/components/app.component.ts
+++ client/dev/vacation-chooser/components/app.component.ts
@@ -2,14 +2,15 @@
  * Created by Lula on 7/3/2016.
  */
 import {Component} from '@angular/core';
-// import { Auth } from '../services/auth.service';
+import { Auth } from '../services/auth.service';
 
 @Component({
   selector: 'vacations-app',
   templateUrl:'vacation-chooser/templates/app.component.html',
+  providers: [ Auth ],
 
 })
 export class AppComponent {
-  // constructor(private auth: Auth) {}
+  constructor(private auth: Auth) {}
   name = "Time traveler";
 }
